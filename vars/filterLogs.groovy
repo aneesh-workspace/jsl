1d0ae9590bfb930dfadaa8c083d0f50794728729
aneesh898 <83596097+aneesh898@users.noreply.github.com>
2021-05-05 21:25:36 +0530

    Update filterLogs.groovy

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -3,6 +3,7 @@
 import org.apache.commons.lang.StringUtils
 
 def call(String filter_string, int occurence) {
+    echo(" Inside JSL filterLogs")
     def logs = CurrentBuild.rawBuild.getLog(10000).join('\n')
     int count = StringUtils.countMatches(logs, filter_string);
     if (count > occurence -1) {
