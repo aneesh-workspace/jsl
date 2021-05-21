243977afbd2f2636547355bb232ea5abd7941d41
aneesh898 <83596097+aneesh898@users.noreply.github.com>
2021-05-21 11:52:13 +0530

    Update filterLogs.groovy

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -6,6 +6,7 @@ def call(String filter_string, int occurence) {
     echo(" Inside JSL filterLogs")
     def logs = currentBuild.rawBuild.getLog(10000).join('\n')
     int count = StringUtils.countMatches(logs, filter_string);
+    println("String ${filter_string} count is ${count}" )
     if (count > occurence -1) {
         currentBuild.result='UNSTABLE'
 }
